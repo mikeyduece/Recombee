a7972b5812d330c28d8165e7287e45b6d25d4195
Mike Heft <mikeheft@gmail.com>
2018-01-22 16:29:32 -0700

    Add test for returning all movies in db from all_movies endpoint

--- spec/requests/item_based_request_spec.rb
+++ spec/requests/item_based_request_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 describe 'Recombee API' do
-  context 'Item Based Recommendations' do
-    xit 'returns list of recommendation for item id' do
+  context 'All Movies' do
+    it 'return list of all movies in database' do
+      movie = Movie.create(id: 117, title: "Robin Hood: Men in Tights (1993)", genre: "Comedy")
+      movie_1  = Movie.create(id: 2997, title: 'Being John Malkovich (1999)', genre: "Comedy")
+      movie_2  = Movie.create(id: 2700, title: 'South Park: Bigger, Longer and Uncut (1999)', genre: "Animation|Comedy")
+      movie_3  = Movie.create(id: 2291, title: 'Edward Scissorhands (1990)', genre: "Drama|Romance")
+      movie_4  = Movie.create(id: 2959, title: 'Fight Club (1999)', genre: "Drama")
+      movie_5  = Movie.create(id: 293, title: 'Professional, The (a.k.a Leon: The Professional) (1994)', genre: "Crime|Drama|Romance|Thriller")
+      movie_6  = Movie.create(id: 1, title: 'Toy Story (1995)', genre: "Animation|Children's|Comedy")
+      movie_7  = Movie.create(id: 3550, title: 'Hunger, The (1983)', genre: 'Horror')
+      movie_8  = Movie.create(id: 3114, title: 'Toy Story 2 (1999)', genre: "Animation|Children's|Comedy")
+      movie_9  = Movie.create(id: 260, title: 'Star Wars: Episode IV - A New Hope (1997)', genre: "Action|Adventure|Sci-Fi")
+      movie_10 = Movie.create(id: 1198, title: 'Raiders of the Lost Ark (1981)', genre: "Action|Adventure")
+      movie_11 = Movie.create(id: 1196, title: 'Star Wars: Episode V - The Empire Strikes Back (1980)', genre: "Action|Adventure|Drama|Sci-Fi|War")
 
+      get '/api/v1/all_movies'
 
+      expect(response).to be_success
+      recs = JSON.parse(response.body)
+
+      expect(recs.count).to eq(12)
+      expect(recs[0]['title']).to eq("Robin Hood: Men in Tights (1993)")
     end
   end
 end
