097b8da1d3f0460bff6c1719d213c3944eeb7341
Mike Heft <mikeheft@gmail.com>
2018-01-22 10:29:28 -0700

    Add validation test for title of movie

--- app/services/recommendation_service.rb
+++ app/services/recommendation_service.rb
@@ -1,9 +1,5 @@
 class RecommendationService
 
-  def initialize(attrs)
-
-  end
-
   def self.get_recomms(movie_id)
     RecombeeService.get_recomms(movie_id)
   end
--- spec/services/recommendation_service_spec.rb
+++ spec/services/recommendation_service_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 describe 'Ratings Servie' do
-  it 'initializes correct service' do
-    movie   = Movie.create(id: 1, title: 'Toy Story (1995)', genre: "Animation|Children's|Comedy")
-    service = RecommendationService.new(movie.id)
-
-    expect(service).to be_a(RecommendationService)
-  end
-
   it 'returns recommendations based on item id' do
     movie   = Movie.create(id: 1, title: 'Toy Story (1995)', genre: "Animation|Children's|Comedy")
     recomms = RecommendationService.get_recomms(movie.id)
