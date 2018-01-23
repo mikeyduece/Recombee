06e8e2036d0fa03887745c5718f1e084a6ef3001
Mike Heft <mikeheft@gmail.com>
2018-01-23 16:59:32 -0700

    Add movie_id to redis rec key to make it more specific

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,11 +2,11 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true
 
   def self.get_recs(movie_id)
-    recs = $REDIS.get('recs')
+    recs = $REDIS.get("recs#{movie_id}")
     if recs.nil?
       recs = RecommendationService.get_recomms(movie_id)
-      $REDIS.set('recs', recs)
-      $REDIS.expire('recs', 3.hour.to_i)
+      $REDIS.set("recs#{movie_id}", recs)
+      $REDIS.expire("recs#{movie_id}", 3.hour.to_i)
     end
     find(JSON.parse(recs.to_s))
   end
