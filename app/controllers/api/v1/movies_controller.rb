64a1461e186b6bb2877242586717d8bf4cbb2101
Mike Heft <mikeheft@gmail.com>
2018-01-24 16:57:45 -0700

    Work on pagination

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MoviesController < Api::V1::ApplicationController
 
   def index
     movies = Movie.all
-    paginate json: movies, per_page: 50
+    paginate json: movies
   end
 end
